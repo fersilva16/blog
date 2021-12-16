bbe137411c6aa1d1a11bbfc20a59c939752dd789
Fernando Silva <fernando457829@gmail.com>
2021-12-16 19:51:44 -0300

    feat: add emotion

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,17 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import '../styles/globals.css';
 
 import type { AppProps } from 'next/app';
+import { Global } from '@emotion/react';
+
+import globalStyle from '../styles/global';
 
 const App = function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Global styles={globalStyle} />
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default App;
