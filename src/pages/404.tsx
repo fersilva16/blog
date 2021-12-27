4119bc3075b717ee9099a367c8466720d4186ab7
Fernando Silva <fernando457829@gmail.com>
2021-12-27 13:34:37 -0300

    feat: add basic pages

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,15 +1,26 @@
 import type { NextPage } from 'next';
+import { Box, Button, Heading } from '@chakra-ui/react';
 import Head from 'next/head';
-import { Box, Heading } from '@chakra-ui/react';
+import NextLink from 'next/link';
 
 const NotFound: NextPage = function NotFound() {
   return (
     <Box display="flex" justifyContent="center" alignItems="center" height="100vh" width="100vw">
       <Head>
-        <title>Not Found!</title>
+        <title>Not Found</title>
       </Head>
 
-      <Heading>Not Found!</Heading>
+      <Box display="flex" justifyContent="center" alignItems="center" flexDirection="column">
+        <Heading as="h1" size="4xl">
+          404
+        </Heading>
+        <Heading as="h2" size="lg" marginTop="6">
+          Ooppss!! Page Not Found!
+        </Heading>
+        <NextLink href="/">
+          <Button marginTop="12">Back to Home!</Button>
+        </NextLink>
+      </Box>
     </Box>
   );
 };
