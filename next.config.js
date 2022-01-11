74a8b91c26cdad8e29609389ed694b88c87a18b2
Fernando Silva <hello@fersilva.dev>
2022-01-11 18:27:10 -0300

    feat: add react-intl

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  i18n: {
+    locales: ['en', 'pt'],
+    defaultLocale: 'en',
+  },
   reactStrictMode: true,
   swcMinify: true,
   experimental: {
