0bb8c6ceb5755bc16a3f8aa0bfcc2f010a809d4d
Fernando Silva <fernandonsilva16@gmail.com>
2022-05-14 08:24:51 -0300

    feat(ui): remove chakra-ui (#185)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,11 @@
 /* eslint-disable react/jsx-props-no-spreading */
 
-import { Box, ChakraProvider } from '@chakra-ui/react';
 import { NextIntlProvider } from 'next-intl';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
+import { Container } from '~/components/Container';
 import { Header } from '~/components/Header';
-import { theme } from '~/styles/theme';
 
 const App = function App({ Component, pageProps }: AppProps) {
   return (
@@ -17,13 +16,11 @@ const App = function App({ Component, pageProps }: AppProps) {
       </Head>
 
       <NextIntlProvider messages={pageProps.messages}>
-        <ChakraProvider theme={theme}>
-          <Header />
+        <Header />
 
-          <Box display="flex" width="100%" paddingLeft="20%" paddingRight="20%">
-            <Component {...pageProps} />
-          </Box>
-        </ChakraProvider>
+        <Container>
+          <Component {...pageProps} />
+        </Container>
       </NextIntlProvider>
     </>
   );
