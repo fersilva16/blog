ff412eab6e47788c4c83b255a9ef4ddce778c8a9
Fernando Silva <fernandonsilva16@gmail.com>
2022-05-21 12:30:56 -0300

    feat(content): improve spacing

--- src/components/ui/Container.tsx
+++ src/components/ui/Container.tsx
@@ -2,6 +2,6 @@ import styled from '@emotion/styled';
 
 export const Container = styled.div`
   display: flex;
-  padding: 0 20%;
-  flex-direction: column;
+  width: 100%;
+  justify-content: center;
 `;
---
+++ src/components/ui/ContentContainer.tsx
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled';
+
+export const ContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 40rem;
+  width: 100%;
+`;
