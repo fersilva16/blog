7d3a7e4bc6774a6cb67497faa8f5c712e06c1c39
Fernando Silva <fernandonsilva16@gmail.com>
2022-06-05 16:08:08 -0300

    feat(error-pages): i18n

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,18 +1,21 @@
+import { useTranslations } from 'next-intl';
 import Head from 'next/head';
 
 import { Center } from '~/components/ui/Center';
 import { getIntlMessagesProps } from '~/lib/intl/getIntlMessagesProps';
 
 const NotFound = () => {
+  const t = useTranslations();
+
   return (
     <>
       <Head>
-        <title>Not Found</title>
+        <title>{t('Not Found')}</title>
       </Head>
 
       <Center>
         <h1>404</h1>
-        <h2>Ooppss!! Page Not Found!</h2>
+        <h2>{t('Ooppss!! Page Not Found!')}</h2>
       </Center>
     </>
   );
--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -1,18 +1,21 @@
+import { useTranslations } from 'next-intl';
 import Head from 'next/head';
 
 import { Center } from '~/components/ui/Center';
 import { getIntlMessagesProps } from '~/lib/intl/getIntlMessagesProps';
 
 const InternalError = () => {
+  const t = useTranslations();
+
   return (
     <>
       <Head>
-        <title>Not Found</title>
+        <title>{t('Internal Server Error')}</title>
       </Head>
 
       <Center>
         <h1>500</h1>
-        <h2>Ooppss!!</h2>
+        <h2>{t('Ooppss!!')}</h2>
       </Center>
     </>
   );
