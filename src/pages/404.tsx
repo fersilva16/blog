964246b0a6917780ff511fbe3618b0dd8934c3aa
Fernando Silva <fernandonsilva16@gmail.com>
2022-06-05 15:56:03 -0300

    feat(error-pages): improve

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head';
-import NextLink from 'next/link';
 
+import { Center } from '~/components/ui/Center';
 import { getIntlMessagesProps } from '~/lib/intl/getIntlMessagesProps';
 
 const NotFound = () => {
@@ -10,9 +10,10 @@ const NotFound = () => {
         <title>Not Found</title>
       </Head>
 
-      <h1>404</h1>
-      <h2>Ooppss!! Page Not Found!</h2>
-      <NextLink href="/">Back to Home!</NextLink>
+      <Center>
+        <h1>404</h1>
+        <h2>Ooppss!! Page Not Found!</h2>
+      </Center>
     </>
   );
 };
