88cf861fed45a4240114f0819f14b017599b9617
Fernando Silva <fernandonsilva16@gmail.com>
2022-06-18 20:16:53 -0300

    feat(home): add post list (#279)

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,12 +1,45 @@
+import styled from '@emotion/styled';
+import { DateTime } from 'luxon';
+import type { GetStaticPropsContext, GetStaticPropsResult } from 'next';
 import { useTranslations } from 'next-intl';
 import Head from 'next/head';
 import Link from 'next/link';
 
 import { Separator } from '../components/ui/Separator';
 import { metadata } from '../data/metadata';
-import { getIntlMessagesProps } from '../lib/intl/getIntlMessagesProps';
+import { getIntlMessages } from '../lib/intl/getIntlMessages';
+import type { GetPostResponse } from '../lib/posts/getPost';
+import { getPosts } from '../lib/posts/getPosts';
 
-const Home = () => {
+export type HomeProps = {
+  posts: Required<GetPostResponse>[];
+};
+
+const PostSeparator = styled.div`
+  border-top: 1px solid #eaeaea;
+`;
+
+const PostPreviewContainer = styled.div`
+  padding: 20px 10px;
+
+  &:hover {
+    background-color: #fafafa;
+    cursor: pointer;
+  }
+`;
+
+const PostPreviewHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+
+const PostPreviewTags = styled.p`
+  margin: 0;
+`;
+
+const Home = ({ posts }: HomeProps) => {
   const t = useTranslations();
 
   return (
@@ -32,11 +65,61 @@ const Home = () => {
           <Separator />
           <Link href={metadata.emailLink}>{t('Email')}</Link>
         </div>
+
+        <div>
+          <h1>Posts</h1>
+          {posts.map(({ slug, frontmatter }, index) => (
+            <>
+              {index !== 0 && <PostSeparator key={index} />}
+              <Link key={slug} href={`posts/${slug}`}>
+                <PostPreviewContainer>
+                  <PostPreviewHeader>
+                    <h2>{frontmatter!.title}</h2>
+
+                    <p>
+                      {DateTime.fromISO(frontmatter!.date).toFormat('DDDD')}
+                    </p>
+                  </PostPreviewHeader>
+
+                  <PostPreviewTags>
+                    {frontmatter!.tags.map((tag) => `#${tag}`).join(', ')}
+                  </PostPreviewTags>
+                </PostPreviewContainer>
+              </Link>
+            </>
+          ))}
+        </div>
       </div>
     </>
   );
 };
 
-export default Home;
+export const getStaticProps = async ({
+  locale,
+}: GetStaticPropsContext): Promise<
+  GetStaticPropsResult<HomeProps & { messages: unknown }>
+> => {
+  const messages = await getIntlMessages(locale);
 
-export const getStaticProps = getIntlMessagesProps();
+  const allPosts = await getPosts();
+
+  const posts = allPosts.filter(
+    ({ frontmatter }) => frontmatter && !frontmatter.draft
+  );
+
+  const sortedPosts = posts.sort((postA, postB) => {
+    const dateA = DateTime.fromISO(postA.frontmatter!.date);
+    const dateB = DateTime.fromISO(postB.frontmatter!.date);
+
+    return dateB.toMillis() - dateA.toMillis();
+  });
+
+  return {
+    props: {
+      posts: sortedPosts,
+      messages,
+    },
+  };
+};
+
+export default Home;
