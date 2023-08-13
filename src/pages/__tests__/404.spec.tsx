d34407c1450cc198e1791c0a940e389ade1848a9
Fernando Silva <fernandonsilva16@gmail.com>
2023-08-13 14:36:05 -0300

    fix(pnpm): fix (#931)

--- src/pages/__tests__/404.spec.tsx
+++ src/pages/__tests__/404.spec.tsx
@@ -1,5 +1,4 @@
 import { screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
 import { getPage } from 'next-page-tester';
 
 it.skip('renders 404 page', async () => {
