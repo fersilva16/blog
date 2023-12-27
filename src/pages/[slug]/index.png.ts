9c8b668a6d5bb0c36a9872f7f3865718d2f3a2db
Fernando Silva <fernandonsilva16@gmail.com>
2023-12-27 11:41:36 -0300

    fix(og): fix font (#1157)

--- src/pages/[slug]/index.png.ts
+++ src/pages/[slug]/index.png.ts
@@ -1,3 +1,4 @@
+import fs from 'node:fs';
 import type { APIRoute } from 'astro';
 import { getCollection, type CollectionEntry } from 'astro:content';
 import satori, { type SatoriOptions } from 'satori';
@@ -21,38 +22,34 @@ const svgBufferToPngBuffer = (svg: string) => {
   const png = resvg.render();
   return png.asPng();
 };
+const cascadiaCodeRegularFont = fs.readFileSync(
+  'public/fonts/CascadiaCode-Regular.ttf',
+).buffer;
 
-export const GET: APIRoute<CollectionEntry<'blog'>> = async ({
-  props,
-  url,
-}) => {
-  const cascadiaCodeRegularFont = await fetch(
-    new URL('/fonts/CascadiaCode-Regular.ttf', url),
-  ).then((res) => res.arrayBuffer());
+const cascadiaCodeBoldFont = fs.readFileSync(
+  'public/fonts/CascadiaCode-Bold.ttf',
+).buffer;
 
-  const cascadiaCodeBoldFont = await fetch(
-    new URL('/fonts/CascadiaCode-Bold.ttf', url),
-  ).then((res) => res.arrayBuffer());
+const options: SatoriOptions = {
+  width: 1200,
+  height: 630,
+  fonts: [
+    {
+      name: 'CascadiaCode',
+      data: cascadiaCodeRegularFont,
+      style: 'normal',
+      weight: 400,
+    },
+    {
+      name: 'CascadiaCode',
+      data: cascadiaCodeBoldFont,
+      style: 'normal',
+      weight: 700,
+    },
+  ],
+};
 
-  const options: SatoriOptions = {
-    width: 1200,
-    height: 630,
-    fonts: [
-      {
-        name: 'CascadiaCode',
-        data: cascadiaCodeRegularFont,
-        style: 'normal',
-        weight: 400,
-      },
-      {
-        name: 'CascadiaCode',
-        data: cascadiaCodeBoldFont,
-        style: 'normal',
-        weight: 700,
-      },
-    ],
-  };
-
+export const GET: APIRoute<CollectionEntry<'blog'>> = async ({ props }) => {
   const svg = await satori(PostOG(props), options);
   const png = svgBufferToPngBuffer(svg);
 
