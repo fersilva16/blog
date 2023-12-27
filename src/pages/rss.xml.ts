cea0af3461b1bd09c5efae3566bcb16f34017a92
Fernando Silva <fernandonsilva16@gmail.com>
2023-12-27 11:14:16 -0300

    feat(rss): improve (#1154)

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,21 +1,27 @@
 import type { APIRoute } from 'astro';
-
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
+import markdownIt from 'markdown-it';
+import sanitizeHtml from 'sanitize-html';
+
 import { config } from '../config';
 
-export const get: APIRoute = async () => {
+const md = markdownIt();
+
+export const GET: APIRoute = async () => {
   const posts = await getCollection('blog', ({ data }) => !data.draft);
 
   return rss({
     title: config.TITLE,
     description: config.DESCRIPTION,
     site: config.SITE_URL,
-    items: posts.map(({ data, slug }) => ({
+    items: posts.map(({ data, slug, body }) => ({
       link: slug,
       title: data.title,
       description: data.description,
       pubDate: data.pubDate,
+      author: config.AUTHOR,
+      content: sanitizeHtml(md.render(body)),
     })),
   });
 };
