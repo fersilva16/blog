44a295a9740a8c7bdc45fff4586ad6a8aa9e43e0
Fernando Silva <fernandonsilva16@gmail.com>
2024-06-30 12:16:16 -0300

    feat(blog): remove vercel

--- astro.config.ts
+++ astro.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel/static';
 
 import { config } from './src/config';
 
@@ -10,7 +9,6 @@ export default defineConfig({
   site: config.SITE_URL,
   integrations: [mdx(), sitemap(), react()],
   output: 'static',
-  adapter: vercel(),
   vite: {
     optimizeDeps: {
       exclude: ['@resvg/resvg-js'],
